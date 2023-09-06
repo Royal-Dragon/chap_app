55e73019c0a9cef6ca1c34b8b8999f0462cea759
Royal-Dragon <b221025@iiit-bh.ac.in>
2023-09-07 02:28:37 +0530

    failed to deploy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,12 @@ import { UserContext, UserContextProvider } from './UserContext';
 import Routes from './Routes';
 import { useContext } from 'react';
 function App() {
-      axios.defaults.baseURL = 'https://chat-app-2ne0.onrender.com';
+      axios.defaults.baseURL = 'http://localhost:4040';
       axios.defaults.withCredentials = true;
       return (
 
             <UserContextProvider>
-                <Routes />
+                  <Routes />
             </UserContextProvider>
 
       )
